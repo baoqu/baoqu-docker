fd5b11df4525f32c25daccbdd91865f19b41ede6
Miguel de la Cruz <mgdelacroix@gmail.com>
2016-03-03 00:39:13 +0000

    Add dockerfile modifications to start the whole docker development environment in the tmux container

--- docker/dev-entrypoint.sh
+++ docker/dev-entrypoint.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-tmux new -s baoqu
+tmux -2 new-session -d -s baoqu
+
+tmux new-window -t baoqu:1 -n 'front'
+tmux select-window -t baoqu:1
+tmux send-keys -t baoqu 'cd app-front' C-m
+tmux send-keys -t baoqu './scripts/figwheel' C-m
+
+tmux new-window -t baoqu:2 -n 'back'
+tmux select-window -t baoqu:2
+tmux send-keys -t baoqu 'cd app-back' C-m
+tmux send-keys -t baoqu 'lein run' C-m
+
+tmux rename-window -t baoqu:0 'layout'
+tmux select-window -t baoqu:0
+tmux send-keys -t baoqu 'cd app-front/layout' C-m
+tmux send-keys -t baoqu 'gulp' C-m
+
+tmux -2 attach-session -t baoqu
